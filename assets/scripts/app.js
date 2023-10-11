a9ad241dc65e17ed4dbdf2a6e0c0d0c986573a8d
Oksana Melnyk <oks.melnuk@gmail.com>
2023-10-11 20:55:46 +0200

    solved

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,13 @@
-const defaultResult = 0;
+const defaultResult = '';
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2
-    return result
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, "");
+    currentResult = ''
 }
 
-currentResult = add(1, 2);
+// currentResult = add(1, 2);
 
-let calculationDeccription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDeccription);
+// let calculationDeccription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+addBtn.addEventListener(('click'), add);
